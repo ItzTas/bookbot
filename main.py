1c54d9693ddb80a059fa811edaf4a6f39d0c0828
ItzTas <talessabini2@gmail.com>
2024-03-04 21:53:25 -0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   main.py git commit -m "creates a dictionary with the number of each letter"

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 with open("books/frankenstein.txt") as f:
     file_contents =f.read()
-    words = file_contents.split()
-    print(len(words))
+    dic_num_letters = {}
+    lower_file_contents = file_contents.lower()
+    for letter in lower_file_contents:
+        if letter.isalpha():
+            if letter in dic_num_letters:
+                dic_num_letters[letter] += 1
+            else:
+                dic_num_letters[letter] = 1
+    print(dic_num_letters)
+    
