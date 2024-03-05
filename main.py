ca710112a477a8bbc6330ac28808bb701af4a7c7
ItzTas <talessabini2@gmail.com>
2024-03-05 15:18:43 -0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   main.py git commit -m "creates a list with the dictionary of each letter and sorts the dictionaries"

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+
 with open("books/frankenstein.txt") as f:
     file_contents =f.read()
     dic_num_letters = {}
+    list_of_dics = []
+    counter = 0
     lower_file_contents = file_contents.lower()
+    num_words = len(file_contents.split())
     for letter in lower_file_contents:
         if letter.isalpha():
             if letter in dic_num_letters:
                 dic_num_letters[letter] += 1
             else:
                 dic_num_letters[letter] = 1
-    print(dic_num_letters)
+                
+    for letter, count in dic_num_letters.items():
+        list_of_dics.append({"letter": letter,"num": count})
+    list_of_dics.sort(key=lambda x: x['num'], reverse=True)
+    print(list_of_dics)
+
     
